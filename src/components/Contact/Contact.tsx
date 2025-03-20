126c8793c0c619fe13bd1d8157c04085235abbf7
Asemanijoshua <joshuaasemani27@gmail.com>
2025-03-20 08:06:45 +0000

    Created the Contact Component and Payment Confirmation Conponent

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,23 @@
+import Navbar from "../Navbar.jsx";
+
+function Contact() {
+    return (
+        <>
+            <Navbar />
+            <section className="my-[25px] ml-[100px] mr-[70px] font-Inter">
+                <h2 className="text-2xl font-semibold font-Inter">Hall Admin Contact Info</h2>
+
+                <hr className="my-[5px] border-gray-200 border" />
+
+                {/* Contact Info */}
+                <div className="flex flex-col gap-2 my-[10px]">
+                    <p>Phone Number: 08100000000</p>
+                    <p>Email: aamusted@gmail.com</p>
+                </div>
+
+            </section>
+        </>
+    )
+}
+
+export default Contact;
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './pages/Home.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import Contact from './components/Contact/Contact.tsx'
+import Payment_Confirmation from './components/Payment_Confirmation/Payment_Confirmation.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <Home />
+    {/* <Home /> */}
+    {/* <Contact /> */}
+    <Payment_Confirmation />
   </BrowserRouter>,
 )
